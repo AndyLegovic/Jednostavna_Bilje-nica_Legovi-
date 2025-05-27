077877572c3f4b8eaac7f24b7e4dcd16e6227ef2
AndyLegovic <andylegovic5@gmail.com>
2025-05-27 19:02:18 +0200

    Add files via upload

---
+++ jednostavna bilježnica.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import messagebox
+
+open("Jednostavna_Bilježnica.txt", "w").close()
+
+def dodaj_biljesku():
+    bilj = unos.get()
+    with open("Jednostavna_Bilježnica.txt", "a") as bilježnica:
+        if bilj.strip() == "":
+            messagebox.showwarning("Upozorenje", "Niste upisali bilješku!")
+            return
+        else:
+            bilježnica.write(bilj + "\n")
+            messagebox.showinfo("Info", "Bilješka je uspješno spremljena")
+
+def prikazi_biljeske():
+        with open("Jednostavna_Bilježnica.txt", "r") as bilježnica:
+            sadrzaj = bilježnica.read()
+            if not sadrzaj.strip():
+                tekst.delete(1.0, tk.END)
+                tekst.insert(tk.END, "Bilježnica je prazna.")
+                return
+            else:
+                tekst.delete(1.0, tk.END)
+                tekst.insert(tk.END, sadrzaj)
+
+prozor = tk.Tk()
+prozor.title("Jednostavna Bilježnica")
+prozor.configure(bg="grey")
+
+unos = tk.Entry(prozor, width=50, bg = "darkgrey")
+unos.pack(padx=10, pady=10)
+
+dodaj_btn = tk.Button(prozor, text="Dodaj bilješku", bg = "darkgrey" , command=dodaj_biljesku)
+dodaj_btn.pack(pady=5)
+
+prikazi_btn = tk.Button(prozor, text="Prikaži bilješke", bg = "darkgrey" , command=prikazi_biljeske)
+prikazi_btn.pack(pady=5)
+
+tekst = tk.Text(prozor, bg="darkgrey" , width=60, height=15)
+tekst.pack(padx=10, pady=10)
+
+prozor.mainloop()
